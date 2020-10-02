b9df07938959e865f2a8fa423a2510ae47d1b169
Vaibhav Agarwal <53995018+va-29@users.noreply.github.com>
2020-10-03 02:09:30 +0530

    Initial commit

---
+++ item.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<stdio.h>
+#include<malloc.h>
+using namespace std;
+class Item
+{
+    string name;
+    int cost;
+public:
+    Item()
+    {
+        this->name="NULL";
+        cost=0;
+    }
+    Item(string name, int cost)
+    {
+        this->name = name;
+        this->cost = cost;
+    }
+    void display()
+    {
+        cout<<"The item details are:"<<endl;
+        cout<<"Item name:"<<name<<endl;
+        cout<<"Item cost:"<<cost<<endl;
+    }
+    void * operator new(size_t size)
+    {
+        cout<<"Overloading new operator"<<endl;
+        void * p=malloc(size);
+        return p;
+    }
+    void operator delete(void * p)
+    {
+       cout<<"Overloading delete operator";
+       free(p);
+    }
+};
+
+
